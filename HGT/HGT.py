a308f86da67a9f3fb36a7662b65778bef6e8fb5a
johnshizhu <johnshizhu@gmail.com>
2023-10-16 22:13:06 -0400

    started implementation

---
+++ HGT/HGT.py
@@ -0,0 +1,13 @@
+'''
+Implementation of Heterogeneous Graph Transformer
+'''
+from ogb_load import Loader
+
+
+class HGT():
+    def __init__(self):
+        
+
+
+        return
+    
---
+++ HGT/ogb_load.py
@@ -0,0 +1,21 @@
+'''
+Faciliates all loading of data using OGB
+
+'''
+from ogb.nodeproppred import PygNodePropPredDataset
+
+# dataset = PygNodePropPredDataset(name='ogbn-mag')
+# split_idx = dataset.get_idx_split()
+# train_idx, valid_idx, test_idx = split_idx["train"], split_idx["valid"], split_idx["test"]
+# graph = dataset[0] # pyg graph object
+
+class Loader():
+
+    def __init__():
+
+
+        return
+    
+    def get_embedding(self):
+
+        return
