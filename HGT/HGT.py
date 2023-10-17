75f3dff4f1857e134081eae7947b5e63cc180f72
johnshizhu <johnshizhu@gmail.com>
2023-10-16 23:50:57 -0400

    first setting up

--- HGT/HGT.py
+++ HGT/HGT.py
@@ -2,11 +2,33 @@
 Implementation of Heterogeneous Graph Transformer
 '''
 from ogb_load import Loader
+import torch
+import torch.nn as nn
+from torch_geometric.nn import GATConv
+from torch_geometric.nn.conv import MessagePassing
+from torch_geometric.utils import softmax
 
 
 class HGT():
+
     def __init__(self):
-        
+        '''
+        Input - graph (Pytorch_Geometric Graph object)
+        '''
+
+        return
+    
+    def mutual_attention(self):
+
+
+        return
+    
+    def message(self):
+
+
+        return
+    
+    def aggregate(self):
 
 
         return
