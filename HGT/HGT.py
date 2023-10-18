c126978543cff9458d1eefbfaec6ecd24d7e3bb1
johnshizhu <johnshizhu@gmail.com>
2023-10-18 02:43:13 -0400

    added more comments

--- HGT/HGT.py
+++ HGT/HGT.py
@@ -1,5 +1,6 @@
 '''
-Implementation of Heterogeneous Graph Transformer
+Implementation of Heterogeneous Graph Transformer (HGT)
+Refer to section 3 of HGT
 '''
 from ogb_load import Loader
 import torch
@@ -7,29 +8,64 @@ import torch.nn as nn
 from torch_geometric.nn import GATConv
 from torch_geometric.nn.conv import MessagePassing
 from torch_geometric.utils import softmax
+from torch_geometric.data import HeteroData
 
 
 class HGT():
-
-    def __init__(self):
+    '''
+    Attention --> estimates the importance of each source node
+    Message --> extracts the message by using only the source node s
+    Aggregate --> aggregates neighborhood message by attention weight
+    '''
+    def __init__(self, input_graph):
         '''
-        Input - graph (Pytorch_Geometric Graph object)
+        Input - input_graph (Pytorch_Geometric Heterogeneous Graph object)
+        '''
+        self.graph = input_graph
+
+        return
+    
+    def layer(self):
+        '''
+
         '''
 
         return
     
-    def mutual_attention(self):
+    def attention(self):
+        '''
+        Heterogeneous Mutual Attention
+        Softmax(a(WH^(l-1)[t] || WH^(l-1)[s]))
+        ∀s ∈ N(t)
+        
+        Given target node t, and all its neightbors=[s ∈ N(t)], which might belon got different distributions,
+        calculate mutual attention grounded by their meta relations ⟨τ (s),ϕ(e), τ (t)⟩
+        
+        Input:
+         - 
+        Output:
+         - attention - Attention embedding
+        
+        '''
 
 
         return
-    
+
     def message(self):
+        '''
+        Heterogeneous Message Passing
+        Message_GAT(s) = WH^(l-1)[s]
 
+        '''
 
         return
     
     def aggregate(self):
+        '''
+        Target-Specific Aggregation
+        Aggregate (Attention(s,t) DOT (Message (s)))
 
+        '''
 
         return
     
